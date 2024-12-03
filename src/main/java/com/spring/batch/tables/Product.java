026da67322b1bad166f692095ddcd24a6a0df333
SSureshC <sureshchandak1@gmail.com>
2024-12-03 16:51:20 +0530

    Create products table

---
+++ src/main/java/com/spring/batch/tables/Product.java
@@ -0,0 +1,28 @@
+package com.spring.batch.tables;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "products")
+public class Product {
+
+    @Id
+    private Integer id;
+
+    private String title;
+
+    private String description;
+
+    private Double price;
+
+    private Float discount;
+
+    private Double discountPrice;
+
+}
